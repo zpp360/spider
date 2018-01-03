d4efc3fad81d0c6bc08d5837e256093c93bb3ce0
zpp360 <184458308@qq.com>
2018-01-03 08:06:41 +0800

    Initial commit

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,5 @@
+# coding:utf-8
+
+class HtmlDownloader(object):
+    def download(self, new_url):
+        pass
---
+++ baike_spider/html_parser.py
@@ -0,0 +1,5 @@
+# coding:utf-8
+
+class HtmlParser(object):
+    def parse(self, new_url, html_cont):
+        pass
